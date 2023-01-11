45bd593242b208f526ebb9c4a5e07ad6457af824
Eleanor Ruth <ellie.ruth@me.com>
2023-01-10 17:33:22 -0700

    Implement React Router

---
+++ src/components/AboutSite/AboutSite.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './AboutSite.css'
+
+const AboutSite = () => {
+  return (
+    <h1>About the Site</h1>
+  )
+}
+
+export default AboutSite
---
+++ src/components/ArtContainer/ArtContainer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './ArtContainer.css'
+
+const ArtContainer = () => {
+  return (
+    <h1>Test art container</h1>
+  )
+}
+
+export default ArtContainer
