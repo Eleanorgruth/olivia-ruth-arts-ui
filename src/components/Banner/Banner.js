48d09867e86a4ee67a1c1b0150f20e3c29723554
Eleanor Ruth <ellie.ruth@me.com>
2023-01-12 09:34:27 -0700

    Add hamburger

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,39 +1,48 @@
 import React, { useState } from "react"
 import { NavLink } from "react-router-dom"
 import './Banner.css'
+import HamburgerMenu from "../HamburgerMenu/HamburgerMenu"
 
 
 const Banner = () => {
   const [selectedItem, setSelectedItem] = useState(null);
+  const [hamburgerOpen, setHamburgerOpen] = useState(false)
+
+  const toggleHamburger = () => {
+    setHamburgerOpen(!hamburgerOpen)
+  }
   return (
     <div>
       <NavLink to={'/'}><h1>Olivia Ruth Arts</h1></NavLink>
-      <nav>
+      <nav className={hamburgerOpen ? 'hide': 'navStyling'} >
         <NavLink
           className={selectedItem === 'glass' ? 'selected menuItem' : 'menuItem'}
           onClick={() => setSelectedItem('glass')}
           to={'/glass'}>Glass</NavLink>
         <NavLink
           className={selectedItem === 'paintings-drawings' ? 'selected menuItem' : 'menuItem'}
-          onClick={() => setSelectedItem('paintings-drawings')} 
+          onClick={() => setSelectedItem('paintings-drawings')}
           to={'/paintings-drawings'}>Paintings and Drawings</NavLink>
         <NavLink
           className={selectedItem === 'print' ? 'selected menuItem' : 'menuItem'}
-          onClick={() => setSelectedItem('print')} 
+          onClick={() => setSelectedItem('print')}
           to={'/print'}>Print Making</NavLink>
         <NavLink
           className={selectedItem === 'fiber' ? 'selected menuItem' : 'menuItem'}
-          onClick={() => setSelectedItem('fiber')} 
+          onClick={() => setSelectedItem('fiber')}
           to={'/fiber'}>Fiber Art</NavLink>
         <NavLink
           className={selectedItem === 'developmental' ? 'selected menuItem' : 'menuItem'}
-          onClick={() => setSelectedItem('developmental')} 
+          onClick={() => setSelectedItem('developmental')}
           to={'/developmental'}>Developmental Art</NavLink>
         <NavLink
           className={selectedItem === 'about-the-site' ? 'selected menuItem' : 'menuItem'}
-          onClick={() => setSelectedItem('about-the-site')} 
+          onClick={() => setSelectedItem('about-the-site')}
           to={'/about-the-site'}>About the Site</NavLink>
       </nav>
+      <div className="hamburger" onClick={toggleHamburger}>
+        <HamburgerMenu isOpen={hamburgerOpen}/>
+      </div>
     </div>
   )
 }
