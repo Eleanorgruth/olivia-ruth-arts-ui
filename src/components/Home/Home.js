d52614dad6809ba84056cb9e738efa147fb243de
Eleanor Ruth <ellie.ruth@me.com>
2023-01-16 10:46:39 -0700

    Fix typo

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -11,7 +11,7 @@ const Home = () => {
         alt="Olivia blowing glass"
       />
       <p className="aboutOlivia">
-        Olivia is a student at Centre College in Danville Kentucky, studying studio arts and gender studies. She has always used art as a tool to investigate her surroundings and make sense of her experience in the world. She grew up in the beautiful Colorado Springs, Colorado and left her hometown to experience something different from what she knew, as well as play field hockey in a d3 setting.
+        Olivia is a student at Centre College in Danville Kentucky, studying studio arts and gender studies. She has always used art as a tool to investigate her surroundings and make sense of her experience in the world. She grew up in the beautiful Colorado Springs, Colorado and left her hometown to experience something different from what she knew, as well as play field hockey in a DIII setting.
         <br/>
         <br/>
         Much of Olivia's art examines ideas around norms, fitting in, entropy, and femininity. At Centre she has learned skills involving glass art and fallen in love with the majesty and mesmerizing nature of molten glass. Olivia is working to challenge herself and her art conceptually and technically through the rest of her degree, as well as hopefully continue her arts education with a Master of Fine Arts after graduating from Centre College.
